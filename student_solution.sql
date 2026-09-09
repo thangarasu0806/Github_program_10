6f83b206a65e9f07708b16a66c49c3e9c1eae4d0
THANGARASU A <thangarasuarul543@gmail.com>
2026-09-09 09:22:36 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,39 @@
+use thangarasudb;
+CREATE TABLE Course(
+courseID INT,
+coursename VARCHAR(30),
+credits int
+);
+
+INSERT INTO Course VALUES
+(201,'DATABASE SYSTEMS',4),
+(202,'DATA STRUCTURES',3),
+(203,'MATHEMATICS',4);
+
+CREATE TABLE Enrollment(
+enrollmentID INT,
+studentID INT,
+courseID INT
+);
+
+INSERT INTO Enrollment VALUES
+(1,1001,201),
+(2,1001,202),
+(3,1002,203),
+(4,1003,201);
+  
+SELECT Course.courseID,
+Course.coursename,
+Enrollment.studentID
+FROM Course
+LEFT JOIN Enrollment
+ON Course.courseID=Enrollment.courseID;
+
+SELECT Course.courseID,
+Course.coursename,
+Enrollment.studentID
+FROM Course
+RIGHT JOIN Enrollment
+ON Course.courseID=Enrollment.courseID;
+
 
